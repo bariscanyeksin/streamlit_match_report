f1f828abfd4cd6cd4ccef4b7ba2cfeadf3749f61
bariscanyeksin <82212277+bariscanyeksin@users.noreply.github.com>
2024-12-13 23:23:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import streamlit as st
 import requests
 import os
 from matplotlib import font_manager as fm
-import matplotlib.image as mpimg
 from matplotlib.colors import to_rgba
 import pandas as pd
 from PIL import Image
@@ -13,6 +12,9 @@ from datetime import datetime
 from mplsoccer import Pitch, add_image
 import base64
 import io
+import json
+import hashlib
+from bs4 import BeautifulSoup
 
 plt.rcParams["figure.dpi"] = 300
 
@@ -103,22 +105,112 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-headers = {
-    'accept': '*/*',
-    'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
-    'cache-control': 'no-cache',
-    'pragma': 'no-cache',
-    'priority': 'u=1, i',
-    'referer': 'https://www.fotmob.com/en-GB/leagues/71/overview/super-lig',
-    'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-    'x-mas': 'eyJib2R5Ijp7InVybCI6Ii9hcGkvbGVhZ3Vlcz9pZD03MSZjY29kZTM9VFVSJm5ld1VlZmFCcmFja2V0PXRydWUiLCJjb2RlIjoxNzMzNTk5MjI2NTUyLCJmb28iOiJlZDk5M2QwYTYifSwic2lnbmF0dXJlIjoiMTMzNTA4RDMxNENCRTE0Njk1N0NCRURGNkFGMTNBRDAifQ==',
-}
+def get_version_number():
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+        'cache-control': 'no-cache',
+        'pragma': 'no-cache',
+        'priority': 'u=0, i',
+        'referer': 'https://www.google.com/',
+        'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+    }
+    
+    response = requests.get("https://www.fotmob.com/", headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    version_element = soup.find('span', class_=lambda cls: cls and 'VersionNumber' in cls)
+    if version_element:
+        return version_element.text.strip()
+    else:
+        return None
+    
+version_number = get_version_number()
+
+def get_xmas_pass():
+    url = 'https://raw.githubusercontent.com/bariscanyeksin/streamlit_radar/refs/heads/main/xmas_pass.txt'
+    response = requests.get(url)
+    if response.status_code == 200:
+        file_content = response.text
+        return file_content
+    else:
+        print(f"Failed to fetch the file: {response.status_code}")
+        return None
+    
+xmas_pass = get_xmas_pass()
+
+def create_xmas_header(url, password):
+        try:
+            timestamp = int(datetime.now().timestamp() * 1000)
+            request_data = {
+                "url": url,
+                "code": timestamp,
+                "foo": version_number
+            }
+            
+            json_string = f"{json.dumps(request_data, separators=(',', ':'))}{password.strip()}"
+            signature = hashlib.md5(json_string.encode('utf-8')).hexdigest().upper()
+            body = {
+                "body": request_data,
+                "signature": signature
+            }
+            encoded = base64.b64encode(json.dumps(body, separators=(',', ':')).encode('utf-8')).decode('utf-8')
+            return encoded
+        except Exception as e:
+            return f"Error generating signature: {e}"
+
+def headers_matchDetails(match_id):
+    api_url = "/api/matchDetails?matchId=" + str(match_id)
+    xmas_value = create_xmas_header(api_url, xmas_pass)
+    
+    headers = {
+        'accept': '*/*',
+        'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+        'cache-control': 'no-cache',
+        'pragma': 'no-cache',
+        'priority': 'u=1, i',
+        'referer': f'https://www.fotmob.com/',
+        'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+        'x-mas': f'{xmas_value}',
+    }
+    
+    return headers
+
+def headers_leagues(league_id):
+    api_url = f"api/leagues?id={league_id}"
+    xmas_value = create_xmas_header(api_url, xmas_pass)
+    
+    headers = {
+        'accept': '*/*',
+        'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+        'cache-control': 'no-cache',
+        'pragma': 'no-cache',
+        'priority': 'u=1, i',
+        'referer': f'https://www.fotmob.com/',
+        'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+        'x-mas': f'{xmas_value}',
+    }
+    
+    return headers
 
 # Sidebar'a görsel ekleme
 image_url = "https://images.fotmob.com/image_resources/logo/leaguelogo/71.png"  # Görselin URL'si
@@ -157,11 +249,12 @@ fig.set_facecolor('#0e1117')
 ax.axis('off')
 
 primary_text_color = '#818f86'
+league_id = 71
 
 # API'den maç verilerini çekmek için bir fonksiyon
 def fetch_finished_matches():
-    api_url = "https://www.fotmob.com/api/leagues?id=71&ccode3=TUR"
-    response = requests.get(api_url, headers=headers)
+    api_url = f"https://www.fotmob.com/api/leagues?id={league_id}&ccode3=TUR"
+    response = requests.get(api_url, headers=headers_leagues(league_id))
     data = response.json()
     allmatches = data['matches']['allMatches']
     
@@ -213,7 +306,7 @@ match_details = next(match for match in matches_by_week[selected_week] if f"{mat
 # Maç detaylarını çekmek için matchId kullan
 match_id = match_details['id']
 match_api_url = f"https://www.fotmob.com/api/matchDetails?matchId={match_id}"
-match_response = requests.get(match_api_url, headers=headers)
+match_response = requests.get(match_api_url, headers=headers_matchDetails(match_id))
 match_data = match_response.json()
 
 general_data = match_data['general']
